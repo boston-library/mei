5d642bd375e256c2f8e289bd71a19077924073f0
Steven Anderson <sanderson@bpl.org>
2016-10-03 05:12:55 -0400

    Remove RestClient to see if it fixes a bug on requesting a resource more than once

--- lib/mei/web_service_base.rb
+++ lib/mei/web_service_base.rb
@@ -1,18 +1,38 @@
-require 'rest_client'
-require 'restclient/components'
-require 'rack/cache'
-
 module Mei
   module WebServiceBase
     attr_accessor :raw_response
 
+    def self.fetch(uri_str, limit = 10)
+      # You should choose better exception.
+      raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+
+      url = URI.parse(uri_str)
+      req = Net::HTTP::Get.new(url.request_uri, { 'User-Agent' => 'Mozilla/5.0 (etc...)', 'Accept' => 'application/json' })
+      response = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') { |http| http.request(req) }
+      case response
+        when Net::HTTPSuccess     then response
+        when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+        else
+          response.error!
+      end
+    end
+
+    def self.get_redirect(uri_str)
+      # You should choose better exception.
+      url = URI.parse(uri_str)
+      req = Net::HTTP::Get.new(url.request_uri, { 'User-Agent' => 'Mozilla/5.0 (etc...)',  'Accept' => 'application/json' })
+      response = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https') { |http| http.request(req) }
+      case response
+        when Net::HTTPRedirection then return response['location']
+        else
+          response.error!
+      end
+    end
+
     # mix-in to retreive and parse JSON content from the web
     def get_json(url)
-      RestClient.enable Rack::Cache
-      r = RestClient.get url, request_options
-      RestClient.disable Rack::Cache
+      r = Mei::WebServiceBase.fetch(url)
       JSON.parse(r)
-
     end
 
     def request_options
